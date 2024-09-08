2119d7ef5b67464ad85d9725787fb986df93bf71
DrVali <ahmadi.world@gmail.com>
2024-09-08 23:49:13 +0330

    Update jq.js

--- js/jq.js
+++ js/jq.js
@@ -1,1 +1,17 @@
+ function oddeven() {
+    const lbl = document.getElementById("result");
+
+     let num1 = document.getElementById("num").value;
+     let entered_number  = num1% 2;
+
+        if(entered_number == 0){
+          alert(` You Enterd ${num1}  That It Is an Even Number. `);
+
+        }
+
+        else{
+            alert(`You Enterd ${num1}  That It Is an Odd Number.`);
+        }
+}
+
 
